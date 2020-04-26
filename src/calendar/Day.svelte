65a21ea2ee6a6eadc062241bd0129890b7879393
Fabrice Basso <fabrice.basso@yahoo.fr>
2020-04-26 13:55:41 +0200

    Calendar : refactor day

--- src/calendar/Day.svelte
+++ src/calendar/Day.svelte
@@ -7,8 +7,8 @@
 	const date = month + '-' + (day < 10 ? '0' : '') + day;
 </script>
 <!-- <div role="gridcell" class="Calendar-day disabled hidden" tabindex="-1" aria-label="Monday, March 30, 2020">30 -->
-<div role="gridcell" class="{className} Calendar-day">
-	<button class="btn btn-day" tabindex="-1" aria-label={label} data-date={date}>{day}</button>
+<div role="gridcell" class="{className} Calendar-day" data-date={date}>
+	<button class="btn btn-day" tabindex="-1" aria-label={label}>{day}</button>
 </div>
 
 <style type="text/scss">
