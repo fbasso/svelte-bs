87b7bd697f7471da7c79b2d5de4d6c851dc2c76b
Fabrice Basso <fabrice.basso@yahoo.fr>
2020-11-29 09:20:12 +0100

    Util : debounce

--- src/util/util.js
+++ src/util/util.js
@@ -5,3 +5,14 @@ export function normalize(value) {
 export function capitalize(str) {
 	return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export const debounce = function(fn, time) {
+	let timer;
+	return (...params) => {
+		if (timer) {
+			clearTimeout(timer);
+		}
+		timer = setTimeout(() => {fn(...params);}, time);
+	};
+};
+
