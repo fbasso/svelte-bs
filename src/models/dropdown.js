bc8dd703d6452ccc679c86d80d8e88438e536c66
Fabrice Basso <fabrice.basso@yahoo.fr>
2021-11-14 10:33:33 +0100

    Next

--- src/models/dropdown.js
+++ src/models/dropdown.js
@@ -2,6 +2,7 @@ import {writable, get} from 'svelte/store';
 
 export function dropdownModel() {
 
+	const isActive = writable(false);
 	const isExpanded = writable(false);
 	const list = writable([]);
 	const highlightedItem = writable(null);
@@ -23,23 +24,53 @@ export function dropdownModel() {
 	}
 
 	return {
+
+		/**
+		 * if, true, isExpanded must be updated depending on the number of items list.
+		 */
+		isActive,
+
+		/**
+		 * Readable store. True if the dropdown is effectively open (list with elements and isActive)
+		 */
 		isExpanded,
+
+		/**
+		 * List of items in the dropdown
+		 */
 		list,
+
+		/**
+		 * Highlighted item on the list, candidate to be selected
+		 */
 		highlightedItem,
+
+		/**
+		 * Selected item.
+		 */
 		selectedItem,
 
+		/**
+		 * Move the highlight item to the next element
+		 */
 		moveDown() {
 			highlightedItem.update(function($highlightedItem) {
 				return moveHighlighted($highlightedItem, 1);
 			});
 		},
 
+		/**
+		 * Move the highlight item to the previous element
+		 */
 		moveUp() {
 			highlightedItem.update(function($highlightedItem) {
 				return moveHighlighted($highlightedItem, -1);
 			});
 		},
 
+		/**
+		 * Selected a element in the list. By default, the highlighted element is considered.
+		 */
 		select(item = get(highlightedItem)) {
 			if (item && get(list).includes(item)) {
 				selectedItem.set(item);
@@ -49,3 +80,4 @@ export function dropdownModel() {
 
 
 }
+
